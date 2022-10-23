08ef50cb4e4dce5b58591da934c8b75d20c505c9
BloodSample <>
2022-10-23 17:45:11 +0400

    Added new Tile sets, Battle system encounter And cinemachine confiner

--- Assets/Player/Player Movement Script/PlayerMovement.cs
+++ Assets/Player/Player Movement Script/PlayerMovement.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb2D;
+    BoxCollider2D myBoxCollider2D;
     public Animator anim;
+    public Animator battleAnim;
+    
+
     
     
     private float speed = 3.0f;
@@ -13,7 +18,10 @@ public class PlayerMovement : MonoBehaviour
 
 
     // Start is called before the first frame update
-  
+    private void Start()
+    {
+        myBoxCollider2D = GetComponent<BoxCollider2D>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -21,6 +29,7 @@ public class PlayerMovement : MonoBehaviour
         movementOfPlayer.x = Input.GetAxisRaw("Horizontal");
         movementOfPlayer.y = Input.GetAxisRaw("Vertical");
         PlayerAnimation();
+        BattleEcnouter();
     }
 
     private void FixedUpdate()
@@ -40,6 +49,28 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = new Vector3(-1f, 1f, 1f);
         }    
+    }
+    void BattleEcnouter()
+    {
+        if (myBoxCollider2D.IsTouchingLayers(LayerMask.GetMask("Tall Grass")))
+        {
+            int battleEcnounterRng = Random.Range(1, 500);
+            Debug.Log("Am walking on grass");
+            if(battleEcnounterRng <= 5)
+            {
+                battleAnim.SetBool("Encounter", true);
+                Debug.Log(battleEcnounterRng);
+                StartCoroutine(LoadScene());
+                Debug.Log("I am supposed to be in the battle scene");
+            }
+             
+        }
+
+    }
+    IEnumerator LoadScene()
+    {
+        yield return new WaitForSeconds(1.1f); 
+        SceneManager.LoadScene(1);
     }
 
 }
